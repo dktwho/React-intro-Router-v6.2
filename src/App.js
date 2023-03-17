0a6bf296745eaea66fcbb17614c6080242d68080
Alexander Korobov <dktwho@mail.ru>
2023-03-17 14:56:46 +0300

    added components and browserRouter, routes, route modified

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='*' element={<h1><NotFound/></h1>}/>
-          <Route path='/' element={<h1><Home/></h1>}/>
-          <Route path='about' element={<h1><About/></h1>}/>
-          <Route path='contacts' element={<h1><Contacts/></h1>}/>
+          <Route path='*' element={<NotFound/>}/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='about' element={<About/>}/>
+          <Route path='contacts' element={<Contacts/>}/>
         </Routes>
         
       </div>
