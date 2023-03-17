17dedb0d8b92eb1a1c1f26c37ebc3d26df886ee5
Alexander Korobov <dktwho@mail.ru>
2023-03-17 14:55:06 +0300

    added components and browserRouter, routes, route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home/Home'
+import About from './About/About'
+import Contacts from './Contacts/Contats'
+import NotFound from './NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='*' element={<h1><NotFound/></h1>}/>
+          <Route path='/' element={<h1><Home/></h1>}/>
+          <Route path='about' element={<h1><About/></h1>}/>
+          <Route path='contacts' element={<h1><Contacts/></h1>}/>
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
